18146d5201a2399084aee320e82eef67bc4ceac8
Meatplay <patrick-haertl@msn.com>
2015-06-25 16:29:13 +0200

    EnemyInstance

--- projects/Game/src/Ememy.h
+++ projects/Game/src/Ememy.h
@@ -1,9 +1,24 @@
 #pragma once
+#include <DirectXMath.h>
+#include <string>
+#include "ConfigParser.h"
+#include <list>
+
 class Ememy
 {
 public:
 	Ememy();
 	~Ememy();
+
+	struct EnemyInstance{
+		DirectX::XMVECTOR pos; //position p in world space
+		DirectX::XMVECTOR vel; //velocity v in world space
+		int hitpoints;
+		std::wstring name;
+		ConfigParser::EnemyType type;
+	};
+
+	std::list<EnemyInstance> g_EnemyInstances;
 
 private:
 	float posX;
