aa64b0b3dca45c9c6f6f67ff31ab53a4d19e5012
Sandr00 <sandro.bauer@gmx.net>
2015-06-25 12:17:32 +0200

    Enemy class added, not filled with life yet

---
+++ projects/Game/Ememy.cpp
@@ -0,0 +1,11 @@
+#include "Ememy.h"
+
+
+Ememy::Ememy()
+{
+}
+
+
+Ememy::~Ememy()
+{
+}
---
+++ projects/Game/Ememy.h
@@ -0,0 +1,17 @@
+#pragma once
+class Ememy
+{
+public:
+	Ememy();
+	~Ememy();
+
+private:
+	float posX;
+	float posY;
+	float posZ;
+	float velocityX;
+	float velocityY;
+	float velocityZ;
+	int remainingHitpoints;
+};
+
